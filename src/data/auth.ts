62d511889909d747b59b71aeecf574111e4e4465
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-12-15 22:20:08 +0800

    🎉 init: auth.repo layer

--- src/data/auth.ts
+++ src/data/auth.ts
@@ -1,4 +1,5 @@
 import { prisma } from  "@/data/connectDb";
+import { User } from "generated/prisma/client";
 
 export const findUserByEmail = async (email: string) => {
 	try {
@@ -9,7 +10,41 @@ export const findUserByEmail = async (email: string) => {
 		})
 		return user;
 	}catch(err) {
-		console.error(err)
+		console.error("Error at findUserByEmail",err)
+		throw err;
+	}
+};
+
+export const findUserById = async (id: string) => {
+	try{
+		const user = await prisma.user.findFirst({
+			where: {
+					id
+			}
+		});
+
+		return user;
+	} catch(err) {
+		console.error("Error at findUserById", err)
 		throw err;
 	}
 }
+
+export const createUser = async (user: User) => {
+	try{
+		const newUser = await prisma.user.create({
+			data: {
+				firstName: user.firstName,
+				lastName: user.lastName,
+				email: user.email,
+				password: user.password,
+				role: user.role
+			}
+		});
+
+		return newUser;
+	}catch(err) {
+		console.error("Error at createUser", err);
+		throw err;
+	}
+};
