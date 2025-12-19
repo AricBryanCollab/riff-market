11d16c116143b4623d93f217100a2b766dc11cd8
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-19 13:53:29 +0800

    🧹chore: remove base-ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,4 @@
-import Input from '@/components/input'
-import Button from '@/components/button'
-import Dialog from '@/components/dialog'
+
 
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -15,9 +13,9 @@ function App() {
       <section className="relative py-20 px-6 text-center overflow-hidden">     
         <h1 className='text-4xl font-semibold text-secondary'>Test Rendering</h1>
         <div className="my-6">
-          <Input/>
+
         </div>
-        <Dialog buttonText='Open It' title="Test Dialog" caption='Hello World. This is a test dialog to confirm if it works' />
+
       </section>
     </div>
   )
