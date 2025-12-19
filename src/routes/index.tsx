cd1cb7b7c67bcd96ad3c4b0b578c343f61089c5c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-19 15:01:17 +0800

    🧩component: dialog

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,15 @@ import { createFileRoute } from '@tanstack/react-router'
 
 import Button from '@/components/button'
 import Input from '@/components/input'
+import Dialog from '@/components/dialog'
+
+import { useDialogStore } from '@/store/dialog'
+
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
- 
+  const { setOpenDialog } = useDialogStore();
 
   return (
     <div className="min-h-screen bg-[#f3f3f2]">
@@ -15,11 +19,12 @@ function App() {
       <section className="relative py-20 px-6 text-center overflow-hidden">     
         <h1 className='text-4xl font-semibold text-secondary'>Test Rendering</h1>
         <div className="my-6">
-          <Button variant="primary">Test Button</Button>
+          <Button action={() => setOpenDialog("test")} variant="primary">Test Button</Button>
         </div>
         <div className="w-fit">
           <Input id="test" label="Test" value="" onChange={() => {}} />
         </div>
+        <Dialog type="test" title="Test Dialog" children={<p>Test Dialog Content</p>} />
       </section>
     </div>
   )
