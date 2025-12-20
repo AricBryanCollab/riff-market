8bde7a785f7302e65e711c236dab8c4241ef2d1b
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-12-20 13:42:20 +0800

    🎉 init: auth query & centralized fetch function

---
+++ src/lib/tanstack-query/fetch.ts
@@ -0,0 +1,19 @@
+export async function apiFetch<T>(
+	input: RequestInfo,
+	init?: RequestInit,
+): Promise<T> {
+	const res = await fetch(input, {
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		...init,
+	});
+
+	if (!res.ok) {
+		const error = await res.json().catch(() => null);
+		throw error ?? new Error(res.statusText);
+	}
+
+	return res.json() as Promise<T>;
+}
