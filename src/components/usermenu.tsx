9a7c5ee3403bcc312463609329246905758766fb
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-12-21 15:54:08 +0800

    🧩 component: user menu appearance when authenticated

--- src/components/usermenu.tsx
+++ src/components/usermenu.tsx
@@ -1,18 +1,60 @@
+import { Bell, ShoppingCart } from "lucide-react";
 import Button from "@/components/button";
 import { useDialogStore } from "@/store/dialog";
 
 const UserMenu = () => {
 	const { setOpenDialog } = useDialogStore();
 
+	const isAuthenticated = false;
+
+	const cartCount = 4;
+	const notificationCount = 2;
+
 	return (
-		<div className="flex items-center gap-3">
-			<Button action={() => setOpenDialog("signin")} variant="outline">
-				Login
-			</Button>
-			<Button action={() => setOpenDialog("signup")} variant="primary">
-				Get Started
-			</Button>
-		</div>
+		<>
+			{isAuthenticated ? (
+				<div className="flex items-center gap-4">
+					<div className="flex size-10 items-center justify-center rounded-xl bg-primary text-white font-bold">
+						R
+					</div>
+
+					{/* Shopping Cart with Badge */}
+					<button type="button" className="relative">
+						<ShoppingCart size={24} className="text-primary" />
+						{cartCount > 0 && (
+							<div className="absolute -top-1 -right-1 flex size-4 items-center justify-center rounded-full bg-rose-500">
+								<span className="text-xs font-semibold text-white leading-none">
+									{cartCount > 9 ? "9+" : cartCount}
+								</span>
+							</div>
+						)}
+					</button>
+
+					{/* Notifications with Badge */}
+					<button type="button" className="relative">
+						<Bell size={24} className="text-primary" />
+						{notificationCount > 0 && (
+							<div className="absolute -top-1 -right-1 flex size-4 items-center justify-center rounded-full bg-rose-500">
+								<span className="text-xs font-semibold text-white leading-none">
+									{notificationCount > 9 ? "9+" : notificationCount}
+								</span>
+							</div>
+						)}
+					</button>
+
+					<Button variant="outline">Logout</Button>
+				</div>
+			) : (
+				<div className="flex items-center gap-3">
+					<Button action={() => setOpenDialog("signin")} variant="outline">
+						Login
+					</Button>
+					<Button action={() => setOpenDialog("signup")} variant="primary">
+						Get Started
+					</Button>
+				</div>
+			)}
+		</>
 	);
 };
 
