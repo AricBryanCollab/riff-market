4262e151ca57530a6a17f22c449cb5ac1d60cca4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-12-22 09:23:11 +0800

    🧪test: use of toast at Button component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import Button from "@/components/button";
-import Dialog from "@/components/dialog";
 import Input from "@/components/input";
 
 import SectionContainer from "@/components/sectioncontainer";
 import Select from "@/components/select";
 import { themeOptions } from "@/constants/themeOptions";
-import { useDialogStore } from "@/store/dialog";
 import { useThemeStore } from "@/store/theme";
+import { useToastStore } from "@/store/toast";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-	const { setOpenDialog } = useDialogStore();
+	const { showToast } = useToastStore();
 	const { theme, setTheme } = useThemeStore();
 
 	return (
@@ -21,7 +20,10 @@ function App() {
 				Test Rendering
 			</h1>
 			<div className="my-6">
-				<Button action={() => setOpenDialog("test")} variant="primary">
+				<Button
+					action={() => showToast("Test Message for the Toast", "error")}
+					variant="primary"
+				>
 					Test Button
 				</Button>
 			</div>
@@ -42,9 +44,6 @@ function App() {
 					withSearchBar
 				/>
 			</div>
-			<Dialog type="test" title="Test Dialog">
-				<p>Sample Dialog is rendered here</p>
-			</Dialog>
 		</SectionContainer>
 	);
 }
