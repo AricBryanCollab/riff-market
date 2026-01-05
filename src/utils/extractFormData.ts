b8556dd805b735de172e6c74f0127264667bcd27
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-05 10:35:13 +0800

    🔧utils: extractPartialFormData

--- src/utils/extractFormData.ts
+++ src/utils/extractFormData.ts
@@ -1,11 +1,30 @@
-export function extractFormData<
-	T extends Record<string, FormDataEntryValue | null>,
->(formData: FormData, fields: string[]): T {
+type FormDataEntry = FormDataEntryValue | null;
+
+export function extractFormData<T extends Record<string, FormDataEntry>>(
+	formData: FormData,
+	fields: string[],
+): T {
 	return fields.reduce(
 		(acc, field) => {
 			acc[field] = formData.get(field);
 			return acc;
 		},
-		{} as Record<string, FormDataEntryValue | null>,
+		{} as Record<string, FormDataEntry>,
 	) as T;
+}
+
+export function extractPartialFormData<T extends Record<string, FormDataEntry>>(
+	formData: FormData,
+	fields: string[],
+): Partial<T> {
+	return fields.reduce(
+		(acc, field) => {
+			const value = formData.get(field);
+			if (value !== null && value !== "") {
+				acc[field] = value;
+			}
+			return acc;
+		},
+		{} as Record<string, FormDataEntry>,
+	) as Partial<T>;
 }
