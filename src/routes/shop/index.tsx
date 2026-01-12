4f452455ee210e3c26a0d153839440a2e0621444
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-12 15:48:23 +0800

    ✨feature: products can be searched using searchTerm state

--- src/routes/shop/index.tsx
+++ src/routes/shop/index.tsx
@@ -1,17 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Search } from "lucide-react";
+import { ListMusic } from "lucide-react";
+import { useState } from "react";
 import AnimatedLoader from "@/components/animatedloader";
 import Button from "@/components/button";
+import ProductActions from "@/components/productactions";
 import ProductCard from "@/components/productcard";
 import SectionContainer from "@/components/sectioncontainer";
 import { Body, H3 } from "@/components/typography";
 import { productsQueryOpt } from "@/routes/shop/route";
+
 export const Route = createFileRoute("/shop/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const [searchTerm, setSearchTerm] = useState("");
 	const {
 		data: productList,
 		isPending,
@@ -48,6 +52,19 @@ function RouteComponent() {
 		);
 	}
 
+	const filteredProducts = productList.filter((product) => {
+		const query = searchTerm.toLowerCase();
+		return (
+			product.name.toLowerCase().includes(query) ||
+			product.brand.toLowerCase().includes(query) ||
+			product.model.toLowerCase().includes(query)
+		);
+	});
+
+	const handleSearchChange = (value: string) => {
+		setSearchTerm(value);
+	};
+
 	return (
 		<SectionContainer>
 			<div className="flex flex-col gap-6 rounded-xl bg-white p-4 md:flex-row md:items-center md:justify-between">
@@ -60,22 +77,10 @@ function RouteComponent() {
 				</div>
 
 				<div className="flex flex-col md:flex-row items-center gap-2">
-					<div className="flex items-center gap-2 border border-slate-300 rounded-lg px-3 py-2">
-						<Search className="h-4 w-4 text-slate-400" />
-						<input
-							type="text"
-							placeholder="Search products"
-							className="outline-none text-sm text-slate-700 placeholder:text-slate-400 bg-transparent w-48"
-						/>
-					</div>
-					{/* <button
-						type="button"
-						onClick={() => navigate({ to: "/product/new" })}
-						className="flex items-center cursor-pointer gap-2 rounded-lg bg-primary px-4 py-2 text-sm font-medium text-white hover:bg-accent transition-colors whitespace-nowrap"
-					>
-						<Plus className="size-4" />
-						<BodySmall>Add Product</BodySmall>
-					</button> */}
+					<ProductActions
+						searchTerm={searchTerm}
+						handleSearchTerm={handleSearchChange}
+					/>
 				</div>
 			</div>
 
@@ -95,9 +100,16 @@ function RouteComponent() {
 			</div>
 
 			<div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-				{productList.map((product) => (
-					<ProductCard key={product.id} product={product} />
-				))}
+				{filteredProducts.length > 0 ? (
+					filteredProducts.map((product) => (
+						<ProductCard key={product.id} product={product} />
+					))
+				) : (
+					<div className="col-span-full flex justify-center items-center gap-4 text-center py-8 text-muted-foreground">
+						<ListMusic size={28} />
+						<p>No products match your search here</p>
+					</div>
+				)}
 			</div>
 
 			<div className="flex justify-center py-6">
