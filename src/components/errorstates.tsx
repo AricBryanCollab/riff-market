d6288d9eb780cc677222b0836666c5f84d1a2e96
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-15 09:58:20 +0800

    🧹chore: rename loading & error state functional components of product for clarity

--- src/components/errorstates.tsx
+++ src/components/errorstates.tsx
@@ -1,16 +1,16 @@
 import Button from "@/components/button";
 
 interface ErrorStateProps {
-	onRetry: () => void;
+	refetch: () => void;
 }
 
-export function ErrorProductState({ onRetry }: ErrorStateProps) {
+export function ProductErrorState({ refetch }: ErrorStateProps) {
 	return (
 		<div className="flex flex-col justify-center items-center w-full min-h-screen gap-4">
 			<p className="text-destructive text-lg my-4">
 				Error gathering the products
 			</p>
-			<Button variant="primary" action={onRetry}>
+			<Button variant="primary" action={refetch}>
 				Try Again
 			</Button>
 		</div>
--- src/components/loadingstates.tsx
+++ src/components/loadingstates.tsx
@@ -1,6 +1,6 @@
 import AnimatedLoader from "@/components/animatedloader";
 
-export function LoadingProductState() {
+export function ProductLoadingState() {
 	return (
 		<div className="flex flex-col items-center py-12">
 			<AnimatedLoader text="Gathering available instruments" />
