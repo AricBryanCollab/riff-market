9bd0070b9a5a85b4c0deb884ded57cedfb99f0e4
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2026-01-17 22:05:00 +0800

    🪝 hook:useUpdateProduct

---
+++ src/hooks/useUpdateProduct.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import useGetProducts from "@/hooks/useGetProducts";
+import type { ProductCategory } from "@/types/enum";
+import type { UpdateProductRequest } from "@/types/product";
+
+const initialProduct = {
+	name: "",
+	category: "ELECTRIC" as ProductCategory,
+	brand: "",
+	model: "",
+	description: "",
+	price: 0,
+	stock: 0,
+};
+
+const useUpdateProduct = (id: string) => {
+	const [product, setProduct] = useState<UpdateProductRequest>(initialProduct);
+	const {
+		product: productData,
+		loadingProduct,
+		isErrorProduct,
+		setSelectedProductId,
+	} = useGetProducts();
+
+	setSelectedProductId(id);
+
+	const onChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => {
+		const value =
+			e.target.type === "number" ? Number(e.target.value) : e.target.value;
+		setProduct({ ...product, [e.target.id]: value });
+	};
+
+	return {
+		productData,
+		loadingProduct,
+		isErrorProduct,
+		onChange,
+	};
+};
+
+export default useUpdateProduct;
