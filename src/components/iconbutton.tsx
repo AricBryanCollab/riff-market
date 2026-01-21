275062ec10ec3168b560efd1a0e2e9b9844c0517
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-21 09:08:57 +0800

    🧩component: enhance icon button styles and feature

--- src/components/iconbutton.tsx
+++ src/components/iconbutton.tsx
@@ -3,19 +3,26 @@ import type { LucideIcon } from "lucide-react";
 interface IconButtonProps {
 	icon: LucideIcon;
 	onClick: () => void;
+	disabled?: boolean;
 	backgroundColor?: string;
 }
 
 const IconButton = ({
 	onClick,
 	icon: Icon,
+	disabled = false,
 	backgroundColor = "bg-primary hover:bg-accent",
 }: IconButtonProps) => {
+	const disabledStyle = "bg-gray-300 text-gray-500 cursor-not-allowed";
+
+	const enabledStyle = `text-background ${backgroundColor} cursor-pointer`;
+
 	return (
 		<button
 			type="button"
+			disabled={disabled}
 			onClick={onClick}
-			className={`size-10 flex items-center justify-center rounded-lg text-background ${backgroundColor}  transition-colors`}
+			className={`size-10 flex  items-center justify-center rounded-lg ${disabled ? disabledStyle : enabledStyle}  transition-colors`}
 		>
 			<Icon size={18} />
 		</button>
