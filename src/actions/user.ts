e098795394f2e846b15b52b10802543d227ca89c
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-22 09:54:33 +0800

    🌐 api: updateUserProfilePictureService

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,9 +1,22 @@
 import { createServerFn } from "@tanstack/react-start";
-import { getAllUsers, getUserById, updateUser } from "@/data/user.repo";
+import {
+	getAllUsers,
+	getUserById,
+	updateProfilePicture,
+	updateUser,
+} from "@/data/user.repo";
+import { env } from "@/env";
 import {
 	type UpdateUserInput,
 	udpateUserSchema,
+	updateProfilePictureSchema,
 } from "@/lib/zod/user.validation";
+import {
+	deleteImage,
+	getPublicId,
+	unsignedUploadImage,
+} from "@/utils/cloudinary";
+import { compressImage } from "@/utils/compressimage";
 import { useAppSession } from "@/utils/session";
 
 export async function getUserByIdService() {
@@ -57,4 +70,74 @@ export async function updateUserService(
 	const updatedUser = await updateUser(userId, data);
 
 	return updatedUser;
+}
+
+export async function updateUserProfilePicService(
+	userId: string,
+	profilePic: File | null,
+) {
+	const existingUser = await getUserById(userId);
+
+	if (!existingUser) {
+		return {
+			error: "User not found",
+		};
+	}
+
+	if (profilePic === null) {
+		if (existingUser?.profilePic) {
+			const publicId = getPublicId(existingUser?.profilePic);
+			await deleteImage(publicId);
+		}
+		await updateProfilePicture(userId, null);
+
+		return profilePic;
+	}
+
+	const parsed = updateProfilePictureSchema.safeParse({
+		profilePic: profilePic,
+	});
+
+	if (!parsed.success) {
+		return {
+			error: "Invalid profile picture",
+			details: parsed.error,
+		};
+	}
+
+	let profPicUrl: string;
+
+	try {
+		const compressedImage = await compressImage({
+			file: profilePic,
+			options: {
+				maxSize: 800,
+				quality: 85,
+				format: "jpeg",
+			},
+		});
+
+		const uploadResult = await unsignedUploadImage({
+			buffer: compressedImage.buffer,
+			filename: profilePic.name,
+			uploadPreset: env.CLOUDINARY_UPLOAD_PRESET,
+		});
+
+		profPicUrl = uploadResult.secure_url;
+
+		if (existingUser?.profilePic) {
+			const publicId = getPublicId(existingUser?.profilePic);
+			await deleteImage(publicId);
+		}
+	} catch (error) {
+		console.error("Error at updateProfilePictureService", error);
+		return {
+			error: "Failed to update the user profile picture",
+			details: error instanceof Error ? error.message : "Internal server error",
+		};
+	}
+
+	await updateProfilePicture(userId, profPicUrl);
+
+	return profPicUrl;
 }
