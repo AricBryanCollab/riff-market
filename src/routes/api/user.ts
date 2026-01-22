6b9d702afe580aa91912445fa65b8b96d460fabb
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-22 10:59:28 +0800

    🌐api: updateUser route handler

--- src/routes/api/user.ts
+++ src/routes/api/user.ts
@@ -1,26 +1,48 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { getUserByIdService } from "@/actions/user";
+import { getUserByIdService, updateUserService } from "@/actions/user";
 import { authMiddleware } from "@/middleware";
 
 export const Route = createFileRoute("/api/user")({
 	server: {
 		middleware: [authMiddleware],
-		handlers: {
-			GET: async ({ context }) => {
-				try {
-					const userId = context.id;
+		handlers: ({ createHandlers }) =>
+			createHandlers({
+				GET: async ({ context }) => {
+					try {
+						const userId = context.id;
 
-					const user = await getUserByIdService(userId);
+						const user = await getUserByIdService(userId);
 
-					return new Response(JSON.stringify(user));
-				} catch (error) {
-					console.error(error);
-					return new Response(
-						JSON.stringify({ message: "Failed to query user data" }),
-						{ status: 500 },
-					);
-				}
-			},
-		},
+						return new Response(JSON.stringify(user));
+					} catch (error) {
+						console.error(error);
+						return new Response(
+							JSON.stringify({ message: "Failed to query user data" }),
+							{ status: 500 },
+						);
+					}
+				},
+
+				PUT: {
+					handler: async ({ request, context }) => {
+						try {
+							const userId = context.id;
+							const rawData = await request.json();
+
+							const updatedUser = await updateUserService(userId, rawData);
+
+							return new Response(JSON.stringify(updatedUser), { status: 200 });
+						} catch (error) {
+							console.error(error);
+							return new Response(
+								JSON.stringify({
+									message: "Failed to update the user data",
+								}),
+								{ status: 500 },
+							);
+						}
+					},
+				},
+			}),
 	},
 });
