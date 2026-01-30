c6adb0111239cf9ee4d35d8cb74ec4153680798e
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-30 15:23:19 +0800

    feat: CategoryGrid usage of data from getProductCountByCategory API

--- src/components/home/CategoryGrid.tsx
+++ src/components/home/CategoryGrid.tsx
@@ -1,14 +1,32 @@
-import type { CategoryMeta } from "@/types/product";
-import CategoryCard from "./CategoryCard";
+import { CategoryGridEmpty } from "@/components/emptystates";
+import { CategoryGridError } from "@/components/errorstates";
+import CategoryCard from "@/components/home/CategoryCard";
+import { CategoryGridLoading } from "@/components/loadingstates";
+import useGetProductCount from "@/hooks/useGetProductCount";
 
-interface CategoryGridProps {
-	categories: CategoryMeta[];
-}
+const CategoryGrid = () => {
+	const {
+		categoryCounts,
+		loadingCategoryCounts,
+		isErrorCategoryCounts,
+		refetchCategoryCounts,
+	} = useGetProductCount();
 
-const CategoryGrid = ({ categories }: CategoryGridProps) => {
+	if (loadingCategoryCounts) {
+		return <CategoryGridLoading />;
+	}
+
+	if (isErrorCategoryCounts) {
+		return <CategoryGridError refetch={refetchCategoryCounts} />;
+	}
+
+	if (!categoryCounts || categoryCounts.length === 0) {
+		return <CategoryGridEmpty />;
+	}
+
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-			{categories.map((category) => (
+			{categoryCounts.map((category) => (
 				<CategoryCard key={category.category} category={category} />
 			))}
 		</div>
