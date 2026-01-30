1d512b0c7e97ef9959b9c4d4d4618e5b87f9348e
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2026-01-30 15:01:59 +0800

    hook: useGetProductCount

---
+++ src/hooks/useGetProductCount.ts
@@ -0,0 +1,33 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { getProductCountByCategory } from "@/lib/tanstack-query/product.queries";
+import type { CategoryMeta, ProductCountByCategoryData } from "@/types/product";
+import { transformProductCategoryCount } from "@/utils/transformProductCategoryCount";
+
+export const prroductCountByCategory = queryOptions<
+	ProductCountByCategoryData[]
+>({
+	queryKey: ["products", "count"],
+	queryFn: getProductCountByCategory,
+});
+
+const useGetProductCount = () => {
+	const {
+		data: rawCategoryCounts,
+		isPending: loadingCategoryCounts,
+		isError: isErrorCategoryCounts,
+	} = useQuery(prroductCountByCategory);
+
+	const categoryCounts = useMemo<CategoryMeta[] | undefined>(() => {
+		if (!rawCategoryCounts) return [];
+		return transformProductCategoryCount(rawCategoryCounts);
+	}, [rawCategoryCounts]);
+
+	return {
+		categoryCounts,
+		loadingCategoryCounts,
+		isErrorCategoryCounts,
+	};
+};
+
+export default useGetProductCount;
